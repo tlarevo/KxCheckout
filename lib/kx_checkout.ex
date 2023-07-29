320f9398c48d0d251fea94da9f62b85eb0118211
Tharindu Abeydeera <tharindu.a@vizuamatix.com>
2023-07-29 21:22:35 +0530

    Initial commit

---
+++ lib/kx_checkout.ex
@@ -0,0 +1,18 @@
+defmodule KxCheckout do
+  @moduledoc """
+  Documentation for `KxCheckout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KxCheckout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kx_checkout_test.exs
@@ -0,0 +1,8 @@
+defmodule KxCheckoutTest do
+  use ExUnit.Case
+  doctest KxCheckout
+
+  test "greets the world" do
+    assert KxCheckout.hello() == :world
+  end
+end
