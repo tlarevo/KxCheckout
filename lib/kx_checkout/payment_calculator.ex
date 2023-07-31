4c1283f83a1200378a8c6f9412a986e3621f7ef2
Tharindu Abeydeera <tharindu.a@vizuamatix.com>
2023-07-31 18:47:19 +0530

    Logic implementation done

--- lib/kx_checkout/payment_calculator.ex
+++ lib/kx_checkout/payment_calculator.ex
@@ -4,39 +4,97 @@ defmodule KxCheckout.PaymentCalculator do
   """
 
   @doc """
-  Calculate the payment for the given shopping list and promotions
+  Calculate the payment for the given shopping list and discount rules
+
+  This function is designed as pure function and has no side-effects
+  it takes inputs and produce output without making any state changes
+  outside of its scope
 
   ## Examples
 
     iex> alias KxCheckout.PaymentCalculator
     iex> shopping_list = [{:GR1, 3.11}, {:SR1, 5.00}, {:CF1, 11.23}, {:GR1, 3.11}]
-    iex> promotions = []
+    iex> promotions = %{}
     iex> PaymentCalculator.calculate(shopping_list, promotions)
     {:ok, 22.45}
 
   """
   @spec calculate(list(tuple()), list()) :: {:ok, float()}
-  def calculate(shopping_list, _promotions) do
-    total =
+  def calculate(shopping_list, discount_rules) do
+    %{net_total: net_total} =
       shopping_list
-      |> count_items_by_prod_code()
-      |> calculate_total()
+      |> calculate_item_counts()
+      |> calculate_item_totals_and_discounts(discount_rules)
+      |> calculate_net_total()
 
-    {:ok, total}
+    {:ok, net_total}
   end
 
-  defp count_items_by_prod_code(shopping_list) do
+  defp calculate_item_counts(shopping_list) do
     Enum.reduce(shopping_list, %{}, fn {code, price}, acc ->
-      Map.update(acc, code, %{count: 1, price: price}, fn item ->
-        %{item | count: item.count + 1, price: item.price}
-      end)
+      Map.update(
+        acc,
+        code,
+        %{count: 1, price: price, total: price, discount: 0.00},
+        fn %{
+             count: count
+           } = item ->
+          %{item | count: count + 1}
+        end
+      )
     end)
   end
 
-  defp calculate_total(item_counts_with_price) do
-    item_counts_with_price
-    |> Enum.reduce(0.00, fn {_k, %{count: count, price: price}}, acc ->
-      count * price + acc
+  defp calculate_item_totals_and_discounts(item_map, discount_rules) do
+    Enum.reduce(item_map, item_map, fn {code, %{count: count, price: price} = details}, acc ->
+      discount =
+        Map.get(discount_rules, code)
+        |> calculate_discount_for_rule({count, price})
+
+      total = count * price
+
+      %{acc | code => %{details | discount: discount, total: total}}
     end)
+  end
+
+  defp calculate_net_total(item_map) do
+    {gross_total, total_discount} =
+      item_map
+      |> Enum.reduce({0.00, 0.00}, fn {_k, %{total: total, discount: discount}},
+                                      {gross_total, total_discount} ->
+        {gross_total + total, total_discount + discount}
+      end)
+
+    net_total = gross_total - total_discount
+
+    %{
+      item_details: item_map,
+      gross_total: gross_total |> Float.round(2),
+      total_discount: total_discount |> Float.round(2),
+      net_total: net_total |> Float.round(2)
+    }
+  end
+
+  defp calculate_discount_for_rule(nil, _), do: 0.00
+
+  defp calculate_discount_for_rule(
+         %{type: :buy_x_get_one, threshold: threshold},
+         {count, price}
+       ) do
+    if count >= threshold + 1, do: price, else: 0.00
+  end
+
+  defp calculate_discount_for_rule(
+         %{type: :price_discount, threshold: threshold, price: price},
+         {count, _}
+       ) do
+    if count >= threshold, do: count * price, else: 0.00
+  end
+
+  defp calculate_discount_for_rule(
+         %{type: :fraction_discount, threshold: threshold, fraction: fraction},
+         {count, price}
+       ) do
+    if count >= threshold, do: count * (price * fraction), else: 0.00
   end
 end
--- test/kx_checkout/payment_calculator_test.exs
+++ test/kx_checkout/payment_calculator_test.exs
@@ -7,10 +7,78 @@ defmodule KxCheckout.PaymentCalculatorTest do
   alias KxCheckout.PaymentCalculator
 
   describe "calculate/2" do
-    test "success: given list of shopping items details and with no promotions applied" do
+    test "success: given shopping items; GR1,SR1,GR1,GR1,CF1" do
       shopping_list = [{:GR1, 3.11}, {:SR1, 5.00}, {:CF1, 11.23}, {:GR1, 3.11}]
-      promotions = []
-      assert({:ok, 22.45} = PaymentCalculator.calculate(shopping_list, promotions))
+      discounts = %{}
+      assert({:ok, 22.45} = PaymentCalculator.calculate(shopping_list, discounts))
     end
+
+    test "success: given shopping item list; GR1,SR1,GR1,GR1,CF1" do
+      shopping_list = [
+        {:GR1, 3.11},
+        {:SR1, 5.00},
+        {:GR1, 3.11},
+        {:GR1, 3.11},
+        {:CF1, 11.23}
+      ]
+
+      discounts = %{
+        :GR1 => %{type: :buy_x_get_one, threshold: 1},
+        :SR1 => %{type: :price_discount, threshold: 3, price: 0.50},
+        :CF1 => %{type: :fraction_discount, threshold: 3, fraction: 1 / 3}
+      }
+
+      assert({:ok, 22.45} = PaymentCalculator.calculate(shopping_list, discounts))
+    end
+  end
+
+  test "success: given shopping items; GR1, GR1" do
+    shopping_list = [
+      {:GR1, 3.11},
+      {:GR1, 3.11}
+    ]
+
+    discounts = %{
+      :GR1 => %{type: :buy_x_get_one, threshold: 1},
+      :SR1 => %{type: :price_discount, threshold: 3, price: 0.50},
+      :CF1 => %{type: :fraction_discount, threshold: 3, fraction: 1 / 3}
+    }
+
+    assert({:ok, 3.11} = PaymentCalculator.calculate(shopping_list, discounts))
+  end
+
+  test "success: given shopping items; SR1,SR1,GR1,SR1" do
+    shopping_list = [
+      {:SR1, 5.00},
+      {:SR1, 5.00},
+      {:GR1, 3.11},
+      {:SR1, 5.00}
+    ]
+
+    discounts = %{
+      :GR1 => %{type: :buy_x_get_one, threshold: 1},
+      :SR1 => %{type: :price_discount, threshold: 3, price: 0.50},
+      :CF1 => %{type: :fraction_discount, threshold: 3, fraction: 1 / 3}
+    }
+
+    assert({:ok, 16.61} = PaymentCalculator.calculate(shopping_list, discounts))
+  end
+
+  test "success: given shopping items; GR1,CF1,SR1,CF1,CF1" do
+    shopping_list = [
+      {:GR1, 3.11},
+      {:CF1, 11.23},
+      {:SR1, 5.00},
+      {:CF1, 11.23},
+      {:CF1, 11.23}
+    ]
+
+    discounts = %{
+      :GR1 => %{type: :buy_x_get_one, threshold: 1},
+      :SR1 => %{type: :price_discount, threshold: 3, price: 0.50},
+      :CF1 => %{type: :fraction_discount, threshold: 3, fraction: 1 / 3}
+    }
+
+    assert({:ok, 30.57} = PaymentCalculator.calculate(shopping_list, discounts))
   end
 end
